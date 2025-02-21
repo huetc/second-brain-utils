457943513be008742a8de3af420f289672a4372d
huetc <christophe.huet.pro@proton.me>
2025-02-22 00:17:39 +0100

    feat: add the entrypoints and finalize the job configurations

--- second_brain_utils/config.py
+++ second_brain_utils/config.py
@@ -14,6 +14,14 @@ class JobConfig(BaseSettings):
     source_directory: str
     target_directory: str
     ref_dt: datetime | None = None
+    mask_private_at_export: bool = True
+    private_ref_filler: str = "_ref not found_"
+    private_section_filler: str = "_section not found_"
+    private_section_start: str = "<PRIVATE>"
+    private_section_end: str = "</PRIVATE>"
+    # These options are currently only passed for the export command
+    export_render_md_links: bool = True
+    export_pop_properties: list[str] | None = None
 
     model_config = SettingsConfigDict(yaml_file=os.getenv("SECOND_BRAIN_UTILS_PARSER_CONF", "config/example.yaml"))
 
--- second_brain_utils/main.py
+++ second_brain_utils/main.py
@@ -2,12 +2,23 @@ import sys
 
 from second_brain_utils.config import JobConfig
 from second_brain_utils.dispatcher import dispatch_notes_by_branch, split_notes_by_branch
+from second_brain_utils.exporter import export_notes
 from second_brain_utils.parser import retrieve_source_notes
+from second_brain_utils.privacy_checker import get_obfuscated_notes
 
 
 def main():
+    print("""
+        This project contains tools to dispatch, export and mask the notes from the second brain.
+
+        Available commands are:
+          - dispatch-notes
+          - export-notes
+    """)
+
+
+def run_dispatch_notes():
     config = JobConfig()
-    print(config)
 
     source_directory = config.source_directory
     if not config.ref_dt:
@@ -21,6 +32,33 @@ def main():
     # Split and dispatch the notes by directory depending on the branches they are associated to
     dispatch_notes_by_branch(
         notes_by_branch=split_notes_by_branch(recent_notes), target_dispatch_dir=target_dispatch_dir
+    )
+
+
+def run_export_notes():
+    config = JobConfig()
+
+    source_directory = config.source_directory
+    target_dump_dir = config.target_directory
+
+    # Parse and retrieve all source notes
+    all_notes = retrieve_source_notes(source_directory=source_directory)
+
+    # Filter the private notes and mask the private sections in the public ones
+    public_notes_with_mask = get_obfuscated_notes(
+        notes=all_notes,
+        private_ref_filler=config.private_ref_filler,
+        private_section_start=config.private_section_start,
+        private_section_end=config.private_section_end,
+        private_section_filler=config.private_ref_filler,
+    )
+
+    # Export the notes
+    export_notes(
+        notes=public_notes_with_mask,
+        target_dir=target_dump_dir,
+        render_md_links=config.export_render_md_links,
+        pop_properties=config.export_pop_properties,
     )
 
 
